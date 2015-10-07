a6a7f8b97bca9fb10a07a72e5d93c442116e57c2
Henry Spindell <henryspindell@gmail.com>
2015-10-06 20:25:59 -0500

    fixed Xcode settings to build on my phone

--- ZombieRunX/OEBridging-Header.h
+++ ZombieRunX/OEBridging-Header.h
@@ -2,15 +2,12 @@
 //  OEBridging-Header.h
 //  ZombieRunX
 //
-//  Created by Scott Cambo on 9/10/15.
-//  Copyright (c) 2015 Scott Cambo. All rights reserved.
+//  Created by Henry Spindell on 10/6/15.
+//  Copyright © 2015 Scott Cambo. All rights reserved.
 //
 
-#ifndef ZombieRunX_OEBridging_Header_h
-#define ZombieRunX_OEBridging_Header_h
-#import <OpenEars/OELanguageModelGenerator.h>
-#import <OpenEars/OEAcousticModel.h>
-#import <OpenEars/OEPocketsphinxController.h>
-#import <OpenEars/OEEventsObserver.h>
+#ifndef OEBridging_Header_h
+#define OEBridging_Header_h
 
-#endif
+
+#endif /* OEBridging_Header_h */
