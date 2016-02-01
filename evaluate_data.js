c14a9c517b595007ff2876c3cdc4eade7d408261
hspindell <henryspindell@gmail.com>
2016-01-31 23:49:38 -0600

    completed skeleton for saving WorldObjects from interaction

--- evaluate_data.js
+++ evaluate_data.js
@@ -1,3 +1,5 @@
+// Script for identifying objects in "speed up/slow down" interactions
+var plotly = require('plotly')("hspindell","obeoge4ez8")
 var Parse = require('parse/node');
 
 PARSE_APP_ID="ZVDFIiZs7dvGqL8eRaKT0mdNMxCwMCzaiTu2yVlO"
@@ -5,7 +7,6 @@ PARSE_CLIENT_KEY="GaGy7ZngizyPHSVVbFy0Q5spus9SJD33NKivzP4m"
 
 PARSE_APP_ID_DEV="fEzVacO5gJMMaZBveiq5WWacZhqacHX6lw3CimcB"
 PARSE_CLIENT_KEY_DEV="XapgjW7Twg14nZD31MMrm8T0Jy0MXmaOqckyuPOV"
-
 
 var args = process.argv.slice(2);
 
@@ -18,19 +19,27 @@ if (args.length < 1) {
   Parse.initialize(PARSE_APP_ID_DEV, PARSE_CLIENT_KEY_DEV);
 }
 
-var plotly = require('plotly')("hspindell","obeoge4ez8")
-
 var DataEvent = Parse.Object.extend("DataEvent");
 var LocationUpdate = Parse.Object.extend("LocationUpdate");
+var WorldObject = Parse.Object.extend("WorldObject");
+
+// ===========================================================
 
 var query = new Parse.Query(DataEvent);
-query.ascending("updatedAt");
-query.equalTo("interaction", "Find stop_sign");
-// query.limit(10); // TODO remove eventually
+var interaction = "Find stop_sign"
+// query.ascending("updatedAt");
+// query.equalTo("interaction", interaction);
+var dataEventId = "askFhUnedq";
+var experienceId;
+var dataLabel;
+
+query.equalTo("objectId", "askFhUnedq");
 
 query.find({
   success:function(dataEvents) {
     for (var i=0; i<dataEvents.length; i++) {
+      experience = dataEvents[0].get("experience");
+      dataLabel = dataEvents[0].get("dataLabel");
       
       var dataQuery = new Parse.Query(LocationUpdate);
       dataQuery.equalTo("experience", dataEvents[i].get("experience"));
@@ -44,14 +53,16 @@ query.find({
           console.log("\nFound " + locationUpdates.length + " updates for data event");
           times = [];
           accels = [];
+          speeds = [];
           
-          if(locationUpdates.length <= 10){
+          if(locationUpdates.length < 22){
             return;
           }
+          
           lastSpeed = locationUpdates[0].get("speed");
           lastTime = locationUpdates[0].get("createdAt");
           
-          times.push(lastTime);
+          times.push(getTimeString(lastTime));
           accels.push(0);
           speeds.push(lastSpeed);
           
@@ -68,45 +79,94 @@ query.find({
             lastSpeed = speed;
             lastTime = time;
             
-            times.push(time);
+            times.push(getTimeString(time));
             accels.push(accel);
             speeds.push(speed);
           }
-          
+
           // graph acceleration over time
           var accelLine = {
             x: times,
             y: accels,
             type: 'line',
-            name: "'acceleration'",
+            name: "acceleration (m/s/s)",
           };
           
           var speedLine = {
             x: times,
             y: speeds,
             type: 'line',
-            name: "'speed'",
+            name: "speed (m/s)",
           };
           
-          var data = [speeds, lines];
-          var layout = {fileopt : "overwrite", filename : "simple-node-example"};
-
-          plotly.plot(data, layout, function (err, msg) {
+          var data = [accelLine, speedLine];
+          var fname = interaction + "_" + locationUpdates[0].get("experience").id;
+          // TODO for filename, use experienceID-interaction combo
+          var layout = {
+            title: interaction + " (Exp. " + locationUpdates[0].get("experience").id + ")",
+            xaxis: {
+              title:"timestamp",
+              titlefont: {
+                family: "Courier New, monospace",
+                size: 18,
+                color: "#7f7f7f"
+              }
+            }
+          };
+          var graphOptions = {layout: layout, filename: fname, fileopt: "overwrite"};
+          plotly.plot(data, graphOptions, function (err, msg) {
           	if (err) return console.log(err);
           	console.log(msg);
           });
           
-          
-          // use algorithm to find index i of accel change
-          // var candidateLocation = locationUpdates[i].get("location");
-          
-          // save dataEvent.get("dataLabel") at candidateLocation
-          
+          //TODO
+          // var CLUSTER_DISTANCE = 0.01; // miles
+          // // use algorithm to find index i of accel change
+          // var candidateIdx = 0;
+          // var candidateLocation = locationUpdates[candidateIdx].get("location");
+          //
+          // var worldObjectQuery = new Parse.Query(WorldObject);
+          // // query WorldObject for object in very close proximity
+          // worldObjectQuery.withinMiles("location", candidateLocation, CLUSTER_DISTANCE);
+          // worldObjectQuery.equalTo("dataLabel", dataLabel);
+          //
+          // // FIXME will there be scope issues with the variables used, such as candidateLocation?
+          // worldObjectQuery.find({
+          //   success: function(worldObjects) {
+          //     if(worldObjects.length == 0) {
+          //       var worldObject = new WorldObject();
+          //       worldObject.set("location", candidateLocation);
+          //       worldObject.set("experience", experience);
+          //       worldObject.set("interaction", interaction);
+          //       worldObject.set("verified", false);
+          //       worldObject.save(null, {
+          //         success: function(worldObject) {
+          //           alert('New WorldObject created with objectId: ' + worldObject.id);
+          //         },
+          //         error: function(worldObject, error) {
+          //           alert('Failed to create new WorldObject, with error code: ' + error.message);
+          //         }
+          //       });
+          //     }
+          //   },
+          //   error: function(error) {
+          //     alert("Error: " + error.code + " " + error.message);
+          //   }
+          // });
         }
       })
     }
   }
 });
+
+function getTimeString(d){
+  seconds = d.getSeconds();
+  minutes = d.getMinutes();
+  hour = d.getHours();
+  milliSeconds = d.getMilliseconds();
+  
+  return [hour,minutes,seconds,milliSeconds].join(':')
+}
 // for each dataEvent in dataEvents
 
   // query all LocationUpdates between dataEvent.startDate and dataEvent.endDate
